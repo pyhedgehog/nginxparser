fc6921f79e8b4d46654affcdb065ebcc8b87a8c7
Dusan Klinec <dusan.klinec@gmail.com>
2017-02-23 14:29:05 +0100

    v0.0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='nginxparser_eb',
-      version='0.0.5',
+      version='0.0.6',
       description='Nginx configuration Parser',
       author='Fatih Erikli',
       author_email='fatiherikli@gmail.com',
